8118f5719cfb655aa1a8277abb00f11a946187cf
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 10:47:52 +0200

    Add project files.

---
+++ CodingZone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodingZone // Note: actual namespace depends on the project name.
+{
+    internal class AreaOfTriangle
+    {
+        static void Main(string[] args)
+        {
+            int baseNum = 0;
+            int heightNum = 0;
+            int calculation = (baseNum * heightNum) / 2;
+
+            Console.WriteLine("Please enter the base of the triangle.");
+            baseNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the height of the triangle.");
+            heightNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(calculation);
+        }
+    }
+}
