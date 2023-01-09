8a6397b77b5d48dee6783487626344d9a43998c6
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 11:55:42 +0200

    Area of Triangle challenge!

---
+++ CodingZone/Very Easy/AreaOfTriangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace AreaOfTriangle; //Change the namespace to CodingZone to run test it from program.cs remember it's a class function so for example this will be AreaOfTriangle.Mainlogic();
+
+public class AreaOfTriangle
+{
+
+
+    public static int TriArea(int b, int h)
+    {
+        return b * h / 2;
+
+    }
+
+
+    public static void MainLogic()
+    {
+        Console.WriteLine("Please enter the base of the triangle");
+        int baseInt = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the height of the triangle");
+        int heightInt = Convert.ToInt32(Console.ReadLine());
+
+        TriArea(baseInt, heightInt);
+        double area = TriArea(heightInt, baseInt); //You can do this??
+        Console.WriteLine($"{area}"); //Learned that $"{}" converts it to an interpolated string (ToString representations of the expressions' results)
+    }
+}
