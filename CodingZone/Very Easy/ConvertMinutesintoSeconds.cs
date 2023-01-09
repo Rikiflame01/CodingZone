d7e4d5923d30055d23be7fcfffac9316fc7b30c0
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 14:48:48 +0200

    Convert minutes into seconds challenge

---
+++ CodingZone/Very Easy/ConvertMinutesintoSeconds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace ConvertMinutesintoSeconds;
+
+public class ConvertMinutesintoSeconds
+{
+
+    public static int convert(int minutes) => minutes*60;
+
+    public static void MainLogic()
+    {
+        Console.WriteLine("Enter amount of minutes to convert");
+        int userinput = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(convert(userinput));
+    }
+}
