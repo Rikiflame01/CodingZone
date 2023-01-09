204d2b5d3bb95ec38ae64d7efa538760f40a0b3d
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 12:46:24 +0200

    Is the number less than zero challenge

---
+++ CodingZone/Very Easy/IsTheNumberLessThanZero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace IsTheNumberLessThanZero;
+
+public class IsTheNumberLessThanZero
+{
+
+    public static bool LessThanOrEqualToZero(double a)
+    {
+        if (a <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void MainLogic()
+    {
+        
+    }
+}
