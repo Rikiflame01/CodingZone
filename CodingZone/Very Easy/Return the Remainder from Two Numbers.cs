ffb2d98dbc224e1c9795f96eea03b55e4239edba
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 14:56:33 +0200

    Return the remainder from two numbers

---
+++ CodingZone/Very Easy/Return the Remainder from Two Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace ReturntheRemainderfromTwoNumbers;
+
+public class ReturntheRemainderfromTwoNumbers
+{
+
+    public static int Remainder(int x, int y) => x % y;
+
+
+    public static void MainLogic()
+    {
+        
+    }
+}
