453d5c9673bcb0d104ee31cfb1779d833d3e56ff
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 15:05:13 +0200

    Adding + 1 to a number

---
+++ CodingZone/Very Easy/Addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace Addition;
+
+public class AdditionOfNum
+{
+
+    public static int Addition(int num) => num + 1;
+
+    public static void MainLogic()
+    {
+        
+    }
+}
