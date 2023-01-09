3e4f835c2297a08e4f4c5c94194b973061e955be
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 12:10:51 +0200

    Convert Age to Days Challenge

---
+++ CodingZone/Very Easy/ConvertAgeToDays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace ConvertAgeToDays;
+
+public class ConvertAgeToDays
+{
+
+    public static int CalcAge(int age)
+    {
+        return age * 365;
+    }
+
+    public static void MainLogic()
+    {
+        
+    }
+}
+
+/*
+Use 365 days as the length of a year for this challenge.
+Ignore leap years and days between last birthday and now.
+Expect only positive integer inputs.
+ */
