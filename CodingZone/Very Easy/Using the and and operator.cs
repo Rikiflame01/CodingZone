89533cbcd06dca10d9337c272029cd03764e8cd8
Damian Grant Grobler <10093515@vgconnect.edu.za>
2023-01-09 15:00:38 +0200

    Boolean operator &&, if x and y are true, return true.

---
+++ CodingZone/Very Easy/Using the and and operator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//2023/1/9
+//Damian Grant Grobler
+namespace usingtheandandOperator;
+
+public class usingtheandandOperator
+{
+
+    public static bool And(bool x, bool y) => x && y; //If x and y are true, return true.
+
+    public static void MainLogic()
+    {
+        
+    }
+}
