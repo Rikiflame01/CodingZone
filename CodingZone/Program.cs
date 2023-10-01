2a61bb4c68986d8a1b2c30027f56ac963e580e2d
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2023-10-01 21:12:39 +0200

    Enum number to month

--- CodingZone/Program.cs
+++ CodingZone/Program.cs
@@ -7,11 +7,11 @@ namespace CodingZone
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my Coding Zone");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-
+            string monthName = NumberToMonth.MonthName(num);
+            Console.WriteLine(monthName);
         }
-        
+       
     }
-
-
 }
